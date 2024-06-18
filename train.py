1aba27e3f899724321a53c6163abbf0e6cc7b389
asueeer <asueeer@163.com>
2024-06-19 01:28:14 +0800

    add

---
+++ train.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+from torch.nn.utils import prune
+import matplotlib.pyplot as plt
+from torchvision.models import resnet18
+
+
+def load_data():
+    # Transform and DataLoader for CIFAR-10
+    transform = transforms.Compose(
+        [transforms.RandomCrop(32, padding=4),
+         transforms.RandomHorizontalFlip(),
+         transforms.ToTensor(),
+         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128,
+                                              shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=100,
+                                             shuffle=False, num_workers=2)
+    return trainloader, testloader
+
+
+def load_resnet18():
+    # Load the pretrained ResNet18 model
+    net = resnet18(pretrained=True)
+    # Replace the final fully connected layer
+    num_ftrs = net.fc.in_features
+    net.fc = nn.Linear(num_ftrs, 10)
+    net = net.to('cuda')
+    return net
+
+
+if __name__ == '__main__':
+    trainloader, testloader = load_data()
+    net = load_resnet18()
+    print("Successfully load resnet18 model")
