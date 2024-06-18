acb4677d14727092b51f2c321091ef2a0ed72b28
asueeer <asueeer@163.com>
2024-06-19 03:16:46 +0800

    add

--- deep-gradient-compression-master/dgc/optim/sgd.py
+++ deep-gradient-compression-master/dgc/optim/sgd.py
@@ -1,55 +1,80 @@
-# modified from https://github.com/pytorch/pytorch/blob/master/torch/optim/sgd.py
-
 import torch
-from torch.optim.optimizer import Optimizer, required
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+from torch.nn.utils import prune
+import matplotlib.pyplot as plt
+from torchvision.models import resnet18
+import argparse
 
-__all__ = ['DGCSGD']
+
+def load_data():
+    # Transform and DataLoader for CIFAR-10
+    transform = transforms.Compose(
+        [transforms.RandomCrop(32, padding=4),
+         transforms.RandomHorizontalFlip(),
+         transforms.ToTensor(),
+         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128,
+                                              shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=100,
+                                             shuffle=False, num_workers=2)
+    return trainloader, testloader
 
 
-class DGCSGD(Optimizer):
-    def __init__(self, params, lr=required, momentum=0, dampening=0,
-                 weight_decay=0, nesterov=False):
-        if lr is not required and lr < 0.0:
-            raise ValueError("Invalid learning rate: {}".format(lr))
-        if momentum < 0.0:
-            raise ValueError("Invalid momentum value: {}".format(momentum))
-        if weight_decay < 0.0:
-            raise ValueError("Invalid weight_decay value: {}".format(weight_decay))
+def load_resnet18():
+    # Load the pretrained ResNet18 model
+    net = resnet18(pretrained=True)
+    # Replace the final fully connected layer
+    num_ftrs = net.fc.in_features
+    net.fc = nn.Linear(num_ftrs, 10)
+    net = net.to('cuda')
+    return net
 
-        defaults = dict(lr=lr, momentum=momentum, dampening=dampening,
-                        weight_decay=weight_decay, nesterov=nesterov)
-        if nesterov and (momentum <= 0 or dampening != 0):
-            raise ValueError("Nesterov momentum requires a momentum and zero dampening")
-        super(DGCSGD, self).__init__(params, defaults)
 
-    def __setstate__(self, state):
-        super(DGCSGD, self).__setstate__(state)
-        for group in self.param_groups:
-            group.setdefault('nesterov', False)
+class Compressor:
+    def __init__(self):
+        print("Compressor Init")
 
-    @torch.no_grad()
+    def compress(self, tensor):
+        sparse = (torch.sum(tensor == 0).item() / tensor.numel()) * 100
+        # print(sparse, tensor.shape)
+        return tensor
+
+    def decompress(self, compressed_tensor):
+        return compressed_tensor
+
+
+class MySGD(optim.SGD):
+    def __init__(self, params, lr=0.01, momentum=0, dampening=0, weight_decay=0, nesterov=False):
+        super(MySGD, self).__init__(params, lr=lr, momentum=momentum, dampening=dampening, weight_decay=weight_decay,
+                                    nesterov=nesterov)
+        self.compressor = Compressor()
+
     def step(self, closure=None):
-        """Performs a single optimization step.
-        Arguments:
-            closure (callable, optional): A closure that reevaluates the model
-                and returns the loss.
-        """
         loss = None
         if closure is not None:
-            with torch.enable_grad():
-                loss = closure()
+            loss = closure()
 
         for group in self.param_groups:
             weight_decay = group['weight_decay']
             momentum = group['momentum']
-            dampening = group['dampening']
             nesterov = group['nesterov']
+            dampening = group['dampening']
+            lr = group['lr']
 
             for p in group['params']:
                 if p.grad is None:
                     continue
+                d_p = p.grad.data
                 if weight_decay != 0:
-                    d_p = weight_decay * p.data
                     if momentum != 0:
                         param_state = self.state[p]
                         if 'momentum_buffer' not in param_state:
@@ -61,11 +86,53 @@ class DGCSGD(Optimizer):
                             d_p = d_p.add(buf, alpha=momentum)
                         else:
                             d_p = buf
-                    d_p = d_p.add(p.grad)
+                    d_p.add_(weight_decay, p.data)
                 else:
-                    d_p = p.grad
+                    d_p = d_p.add(p.grad)
 
-                p.add_(d_p, alpha=-group['lr'])
+                compressed_d_p = self.compressor.compress(d_p)
+                p.data.add_(-lr, compressed_d_p)
 
         return loss
 
+
+def train_model(epoch, net, trainloader, optimizer, criterion):
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to('cuda'), targets.to('cuda')
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+    accuracy = 100. * correct / total
+    print(f'Epoch {epoch}, Loss: {train_loss / len(trainloader)}, Accuracy: {accuracy}')
+    return accuracy
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+    parser.add_argument('--compress', default=False, type=bool, help='compress')
+    args = parser.parse_args()
+
+    trainloader, testloader = load_data()
+    net = load_resnet18()
+    print("Successfully load resnet18 model")
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4)
+    if args.compress:
+        optimizer = MySGD(net.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=50, gamma=0.1)
+
+    for epoch in range(5):
+        train_acc = train_model(epoch, net, trainloader, optimizer, criterion)
+        scheduler.step()
