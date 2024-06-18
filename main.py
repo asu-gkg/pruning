a612ba0f8b3482e70f54e80fa20c80c5ce4ec97c
asueeer <asueeer@163.com>
2024-06-19 01:28:05 +0800

    add

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import argparse
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+parser = argparse.ArgumentParser()
+parser.add_argument("--nnodes", type=int)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    args = parser.parse_args()
+    nnodes = args.nnodes
+    print(nnodes)
